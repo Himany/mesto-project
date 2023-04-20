39c3e864c2d6685b1e52022da6738f8fe1181409
Himany <Himaaan@yandex.ru>
2023-04-20 18:22:37 +0300

    revert last 3 commits

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,66 +1,67 @@
-export default class Api {
-  constructor(options) {
-    this._baseUrl = options.baseUrl;
-    this._cardsUrl = options.cardsUrl;
-    this._cardLikesUrl = options.cardLikesUrl;
-    this._profileUrl = options.profileUrl;
-    this._avatarUrl = options.avatarUrl;
-
-    this._headers = {
-      authorization: options.authorization
-    }
-    this._headersJson = {
-      authorization: options.authorization,
-      'Content-Type': 'application/json'
-    }
+const config = {
+  baseUrl: 'https://nomoreparties.co/v1/plus-cohort-22',
+  getCardsUrl: '/cards',
+  getCardLikesUrl: '/cards/likes',
+  getProfileUrl: '/users/me',
+  getAvatarUrl: '/users/me/avatar',
+  headers: {
+    authorization: '9c3089de-d2f6-4f94-8f18-89f9e74580b3'
+  },
+  headersJson: {
+    authorization: '9c3089de-d2f6-4f94-8f18-89f9e74580b3',
+    'Content-Type': 'application/json'
   }
+}
 
-  _serverQuery(inUrl, inMethod = 'GET', data) {
-    let options = {
+function serverQuery(inUrl, inMethod = 'GET', data) {
+  let options = {
+    method: inMethod,
+    headers: config.headers
+  }
+  if (data) {
+    options = {
       method: inMethod,
-      headers: this._headers
+      headers: config.headersJson,
+      body: JSON.stringify(data)
     }
-    if (data) {
-      options = {
-        method: inMethod,
-        headers: this._headersJson,
-        body: JSON.stringify(data)
-      }
-    }
-    return fetch(this._baseUrl + inUrl, options)
-    .then((result) => {
-      if (result.ok) {
-        return(result.json());
-      }
-      throw new Error(result.status);
-    });
   }
+  return fetch(config.baseUrl + inUrl, options)
+  .then((result) => {
+    if (result.ok) {
+      return(result.json());
+    }
+    throw new Error(result.status);
+  });
+}
 
-  //cards
-  getInitialCards() {
-    return this._serverQuery(this._cardsUrl);
-  }
-  addCard(data) {
-    return this._serverQuery(this._cardsUrl, 'POST', data);
-  }
-  deleteCard(cardId) {
-    return this._serverQuery(this._cardsUrl + "/" + cardId, 'DELETE');
-  }
-  addCardLike(cardId) {
-    return this._serverQuery(this._cardLikesUrl + "/" + cardId, 'PUT');
-  }
-  deleteCardLike(cardId) {
-    return this._serverQuery(this._cardLikesUrl + "/" + cardId, 'DELETE');
-  }
-  
-  //user
-  getProfileData() {
-    return this._serverQuery(this._profileUrl);
-  }
-  setProfileData(data) {
-    return this._serverQuery(this._profileUrl, 'PATCH', data);
-  }
-  updateAvatar(data) {
-    return this._serverQuery(this._avatarUrl, 'PATCH', data);
-  }
+export function getInitialCards() {
+  return serverQuery(config.getCardsUrl);
+}
+
+export function getProfileData() {
+  return serverQuery(config.getProfileUrl);
+}
+
+export function setProfileData(data) {
+  return serverQuery(config.getProfileUrl, 'PATCH', data);
+}
+
+export function addCard(data) {
+  return serverQuery(config.getCardsUrl, 'POST', data);
+}
+
+export function deleteCard(cardId) {
+  return serverQuery(config.getCardsUrl + "/" + cardId, 'DELETE');
+}
+
+export function addCardLike(cardId) {
+  return serverQuery(config.getCardLikesUrl + "/" + cardId, 'PUT');
+}
+
+export function deleteCardLike(cardId) {
+  return serverQuery(config.getCardLikesUrl + "/" + cardId, 'DELETE');
+}
+
+export function updateAvatar(data) {
+  return serverQuery(config.getAvatarUrl, 'PATCH', data);
 }
