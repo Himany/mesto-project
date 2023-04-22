d9e435cba986546220fe5ffcafce074aef559dfa
VLGrigoryan <v.grigoryan@live.com>
2023-04-22 14:12:54 +0200

    fix-popupclass

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -4,19 +4,20 @@ export default class Popup {
 
     this._popup = document.querySelector(popupSelector);
     this._closeButton = this._popup.querySelector('.popup__close');
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
   open() {
     this._popup.classList.add('popup_opened');
-    document.addEventListener('keydown', this._handleEscClose.bind(this));
+    document.addEventListener('keydown', this._handleEscClose);
   }
   close() {
     this._popup.classList.remove('popup_opened');
-    document.removeEventListener('keydown', this._handleEscClose.bind(this));
+    document.removeEventListener('keydown', this._handleEscClose);
   }
   _handleOverlay(evt) {
     if (evt.target.classList.contains('popup')) {
       this.close();
-    };
+    }
   }
   _handleEscClose(evt) {
     if (evt.key === "Escape") {
